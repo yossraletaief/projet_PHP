628be027c256fa15d982fc77a0841cc519f66b10
yossra letaief <yossraletaief@gmail.com>
2022-12-09 21:42:46 +0100

    last modifications

--- login.php
+++ login.php
@@ -35,8 +35,8 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
+                           
+                            <div class="col-lg-12">
                                 <div class="p-5">
                                     <div class="text-center">
                                         <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
@@ -58,7 +58,7 @@
                                                     Me</label>
                                             </div>
                                         </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                        <a href="user_interface.php" class="btn btn-primary btn-user btn-block">
                                             Login
                                         </a>
                                         <hr>
